e440406798e3b2a1faba38facec35c16ca264f17
beststudymonu <73981682+beststudymonu@users.noreply.github.com>
2020-11-07 22:10:58 +0530

    Create index.php

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ $message = file_get_contents('php://input');
 require 'libs/Telegram.php';
 require 'libs/Youtube.php';
 
-use Smoqadam\Telegram;
-use Smoqadam\Youtube;
+use Smoqadam\Telegram;1498613767:AAHY_7MCd4k4F-yprxyfm5dx_-9S_J9TMio
+use Smoqadam\Youtube;1498613767:AAHY_7MCd4k4F-yprxyfm5dx_-9S_J9TMio
 
-$api_token = 'API_TOKEN';
+$api_token = '1498613767:AAHY_7MCd4k4F-yprxyfm5dx_-9S_J9TMio';
 
-$tg = new Telegram($api_token);
+$tg = new Telegram($api_token);1498613767:AAHY_7MCd4k4F-yprxyfm5dx_-9S_J9TMio
 $y = new Youtube();
-
+1498613767:AAHY_7MCd4k4F-yprxyfm5dx_-9S_J9TMio
 
 /**
  * download a video by video Id
- */
+ */1037565910
 $tg->cmd('vid:<<:any>>', function ($video_id, $option) use ($tg, $y, $message) {
 
 
-    if (!strlen($video_id)) {
+    if (!strlen($video_id)) {1037565910
 
         $tg->sendMessage("vid:<youtube video ID>", $tg->getChatId());
         return;
